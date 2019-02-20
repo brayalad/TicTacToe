8887d6756cc7ab5ba405adeb45efcf77d4f3b2d2
brayalad <brayalad@yahoo.com>
2019-02-20 14:39:19 -0800

    working on showing changes after turn

--- TicTacToeGUI.py
+++ TicTacToeGUI.py
@@ -8,27 +8,41 @@ window.geometry("312x324") # size of the window width:- 500, height:- 375
 window.resizable(0, 0) # this prevents from resizing the window
 window.title("Tic Tac Toe")
 
+
+def player_turn():
+    gameBoard[0][0] = "X"
+    tkinter.Label(window, text = "HI").pack()
+
+
 tkinter.Label(window, text="1").grid(row = 0)
 
-tkinter.Button(window, text="*").grid(row=0,column=1)
-tkinter.Button(window, text="*").grid(row=0,column=2)
-tkinter.Button(window, text="*").grid(row=0,column=3)
+upperLeftCorner = tkinter.Button(window, text=gameBoard[0][0], command=player_turn).grid(row=0,column=1)
+upperMiddle = tkinter.Button(window, text=gameBoard[0][1]).grid(row=0,column=2)
+upperRightCorner = tkinter.Button(window, text=gameBoard[0][2]).grid(row=0,column=3)
 
 
 tkinter.Label(window, text="2").grid(row = 1)
 
-tkinter.Button(window, text="*").grid(row=1,column=1)
-tkinter.Button(window, text="*").grid(row=1,column=2)
-tkinter.Button(window, text="*").grid(row=1,column=3)
+centerLeft = tkinter.Button(window, text=gameBoard[1][0]).grid(row=1,column=1)
+centerMiddle = tkinter.Button(window, text=gameBoard[1][1]).grid(row=1,column=2)
+centerRight = tkinter.Button(window, text=gameBoard[1][2]).grid(row=1,column=3)
 
 
 tkinter.Label(window, text="3").grid(row = 2)
 
-tkinter.Button(window, text="*").grid(row=2,column=1)
-tkinter.Button(window, text="*").grid(row=2,column=2)
-tkinter.Button(window, text="*").grid(row=2,column=3)
+lowerLeftCorner = tkinter.Button(window, text=gameBoard[2][0]).grid(row=2,column=1)
+lowerMiddle = tkinter.Button(window, text=gameBoard[2][1]).grid(row=2,column=2)
+lowerRightCorner = tkinter.Button(window, text=gameBoard[2][2]).grid(row=2,column=3)
 
 
-tkinter.Button(window, text="*").pack()
+
+
+
+
+
+
+
+
+
 
 window.mainloop()
