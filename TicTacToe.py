4c8cfee4c94f829fda7194843bad8e4e6e335d86
Bryan Ayala <brayalad@yahoo.com>
2019-02-24 00:19:14 -0800

    Fixed main loop

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,24 +1,25 @@
 
 
 def beginProgram():
-    while True:
-        print("\nChoose a Tic-Tac-Toe Game To Play: ")
-        print(" 1. Two-Player Text User Interface")
-        print(" 2. Two-Player Graphical User Interface")
-        print(" 3. AI Text User Interface")
-        print(" 4. AI Graphical User Interface")
-        answer = int(input("Enter choice: "))
+    
+    print("\nChoose a Tic-Tac-Toe Game To Play: ")
+    print(" 1. Two-Player Text User Interface")
+    print(" 2. Two-Player Graphical User Interface")
+    print(" 3. AI Text User Interface")
+    print(" 4. AI Graphical User Interface")
+    answer = int(input("Enter choice: "))
 
-        if answer == 1:
-           import TicTacToeText
-        elif answer == 2:
-            import TicTacToeGUI
-        elif answer == 3:
-            import TicTacToeTextAI
-        elif answer == 4:
-            import TicTacToeGUIAI
-        else:
-            print("Invalid Input")
-            print("Please Try Again")
+    if answer == 1:
+        import TicTacToeText
+    elif answer == 2:
+        import TicTacToeGUI
+    elif answer == 3:
+        import TicTacToeTextAI
+    elif answer == 4:
+        import TicTacToeGUIAI
+    else:
+        print("Invalid Input")
+        print("Please Try Again")
+        beginProgram()
 
 beginProgram()
